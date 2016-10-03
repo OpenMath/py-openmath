dc1e4400e68bacb86c646a21a6b8edf5ff588d46
Luca De Feo <defeo@pippin>
2016-10-03 17:05:33 +0200

    PyPI metadata

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openmath',
+    version='0.1.0a2',
+    description='Object implementation of the OpenMath standard',
+    url='https://github.com/OpenMath/py-openmath',
+    author='Luca De Feo, Tom Wiesing',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='openmath',
+    packages=find_packages(),
+    install_requires=['lxml', 'case_class'],
+)
